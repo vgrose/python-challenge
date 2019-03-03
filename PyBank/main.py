bb7c7e1bdfd657f11f2399d37d142a6b00f32d1c
Victoria Grose <victoria.grose@gmail.com>
2019-03-03 16:52:42 -0600

    cleaned up formatting and exported results to texts files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,44 +1,61 @@
 #Import necessary:
 import os
 import csv
+
 #Set file location:
 csvpath= os.path.join("budget_data.csv")
+
+#Open file
 with open(csvpath) as csvfile:
+
     #Separate each column at comma:
     budget_data = csv.reader(csvfile, delimiter=',')
+
     #Skip header:
     csv_header = next(budget_data)
+
     #Set initial values to zero:
     total_months=0
     net_total=0
     greatest_increase=0
     greatest_decrease=0
+
     #Iterate through rows in imported csv file:
     for row in budget_data:
+
         #Add up the total months:
         total_months=total_months+1
+
         #Name variables:
         profit=int(row[1])
         month=row[0]
+
         #Add up the total profits:
         net_total=net_total+profit
+
         #Set initial profit:
         if total_months==1:
             initial_profit=profit
+
         #Find the greatest profit increase:
         if profit>greatest_increase:
             greatest_increase=profit
             greatest_increase_month=month
+
         #Find greatest profit decrease:
         if profit<greatest_decrease:
             greatest_decrease=profit
             greatest_decrease_month=month
+
         #Set final profit:
         final_profit=profit
+
     #Calculate average profit:
     average_profit=(final_profit-initial_profit)/(total_months-1)
+
     #Format average profit like moneys:
     average=round(average_profit,2)
+
     #Print results:
     print("")
     print("Financial Analyis")
@@ -55,5 +72,15 @@ with open(csvpath) as csvfile:
     print("")
     print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
     print("")
+    print("---------------------------------")
+    print("")
 
-   
+    #Print results to text file and close:         
+    text_file = open("PyBank_Output.txt", "w")
+    text_file.writelines(["Financial Analyis \n","-------------- \n",
+        f"Total Months: {total_months} \n", f"Total: ${net_total} \n", 
+            f"Average Change: ${average} \n", 
+                f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase}) \n", 
+                    f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease}) \n",
+                        "--------------"  ])
+    text_file.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,44 @@
 #Import necessary:
 import os
 import csv
+
 #Set file location:
 csvpath= os.path.join("election_data.csv")
+
+#Open file:
 with open(csvpath) as csvfile:
+
     #Separate each column at comma:
     election_data = csv.reader(csvfile, delimiter=',')
+
     #Skip header:
     csv_header = next(election_data)
+
     #Set initial votes to zero and create an empty dictionary for candidates:
     total_votes=0
     candidate_votes={}
+
     #Iterate over rows:
     for row in election_data:
+
         #Add up total votes:
         total_votes=total_votes+1
+
+        #Name variable:
         candidate=row[2]
+
+        #Put first vote for candidate in dictionary:
         if candidate not in candidate_votes:
-            
             candidate_votes[candidate]=1
+
+        #Add additional votes to dictionary:
         else:
             candidate_votes[candidate]=candidate_votes[candidate]+1
+
+    #Set initial variable to zero:
     winning_votes=0
 
-
-
+    #Print title and total votes:
     print("")
     print("Election Results")
     print("")
@@ -33,6 +47,16 @@ with open(csvpath) as csvfile:
     print(f"Total Votes: {total_votes}")
     print("")
     print("----------------------------------------")
+
+    #Print title and total votes to text file:
+    text_file = open("PyPoll_Output.txt", "w")
+    text_file.writelines(["Election Results \n", "---------------- \n", f"Total Votes: {total_votes} \n",
+        "---------------- \n"])
+    text_file.close()
+
+    #Iterate through candidate dictionary to calculate and print percent votes:
+    #Also append text file:
+    edit_text=open("PyPoll_Output.txt", "a")
     for candidate in candidate_votes:
         votes=candidate_votes[candidate]
         percent_votes=(votes/total_votes)*100
@@ -40,19 +64,22 @@ with open(csvpath) as csvfile:
         print("")
         print(f"{candidate}: {percent_votes}% ({candidate_votes[candidate]})")
         print("")
+        edit_text.writelines([f"{candidate}: {percent_votes}% ({candidate_votes[candidate]}) \n"])
+
+        #Also find winner:
         if votes>winning_votes:
             winning_votes=votes
             winner=candidate
-    
-    print("--------------------------------------")
-    print("")
-    print (f"Winner: {winner}")
-    print("")
-    print("-------------------------------------")
 
-    
-#The total number of votes cast
-#A complete list of candidates who received votes
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#The winner of the election based on popular vote.
+    #Print final result:
+    print("----------------------------------------")
+    print("")
+    print (f"The winner is {winner}!")
+    print("")
+    print("----------------------------------------")
+    print("")
+
+    #Add final result to text file and close:
+    edit_text.writelines(["---------------- \n", f"The winner is {winner}! \n", "---------------- \n" ])
+    edit_text.close()
+
